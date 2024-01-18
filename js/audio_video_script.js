c7b76292814f0f2816b3b4f673a36d7fc2f4b823
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2024-01-18 17:00:34 -0400

    Dropdown funcionando audiovisual

---
+++ js/audio_video_script.js
@@ -0,0 +1,27 @@
+function toggleTable_audio_visual_oferta1(){
+    var table = document.getElementById("audio_visual_oferta1");
+    if (table.style.display === "none"){
+        table.style.display = "block";
+    }
+    else {
+        table.style.display = "none";
+    }
+}
+function toggleTable_audio_visual_oferta2(){
+    var table = document.getElementById("audio_visual_oferta2");
+    if (table.style.display === "none"){
+        table.style.display = "block";
+    }
+    else {
+        table.style.display = "none";
+    }
+}
+function toggleTable_audio_visual_oferta3(){
+    var table = document.getElementById("audio_visual_oferta3");
+    if (table.style.display === "none"){
+        table.style.display = "block";
+    }
+    else {
+        table.style.display = "none";
+    }
+}
