06b761e6e775cb41cb9ca4046c58f1f829f9e8fe
jotafreitas <joaocarlofreitas@hotmail.com>
2024-02-20 16:35:37 -0400

    Menu Sanduiche funcionando
    criei um arquivo js, um arquivo html e acrescentei no css

---
+++ js/sanduiche.js
@@ -0,0 +1,5 @@
+function onClickMenuSand() {
+    document.getElementById("menu-sand").classList.toggle("change");
+    document.getElementById("nav-sand").classList.toggle("change");
+    document.getElementById("menu-bg-sand").classList.toggle("change")
+}
