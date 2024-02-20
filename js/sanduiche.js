84e73d0c75669a6232e5120c8e6e4d51375f0827
jotafreitas <joaocarlofreitas@hotmail.com>
2024-02-20 16:55:55 -0400

    Menu Sanduíche - parcialmente pronto

--- js/sanduiche.js
+++ js/sanduiche.js
@@ -1,5 +1,5 @@
 function onClickMenuSand() {
     document.getElementById("menu-sand").classList.toggle("change");
-    document.getElementById("nav-sand").classList.toggle("change");
-    document.getElementById("menu-bg-sand").classList.toggle("change")
+    document.getElementById("nav-sand").classList.toggle("change-sand");
+    document.getElementById("menu-bg-sand").classList.toggle("change-sand")
 }
