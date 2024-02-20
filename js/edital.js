831f23a1b250dc24c129a66b11c8926f18b4d791
Dudu1705 <eduardoromanoamaral11@gmail.com>
2024-02-20 17:12:15 -0400

    Feature_70
    Tentamos fazer o menudropdow,mais não esta dando certo

--- js/edital.js
+++ js/edital.js
@@ -1,16 +1,45 @@
+
+
 document.addEventListener('DOMContentLoaded', function () {
-    var table = document.getElementById("inscricoes");
-    if (table) {
-        table.style.display = "none";  // Initial state: hidden
+    var edital = document.getElementById("inscricoes1");
+    if (edital) {
+        edital.style.display = "none";  // Initial state: hidden
     }
 });
 
-function toggleTable_inscricoes1() {
-    var seta = document.getElementById("inscricoes");
+function toggletabble_inscricoes1() {
+    var ins1 = document.getElementById("inscricoes1");
+    var ins2 = document.getElementById("inscricoes2");
+    console.log("oi")
 
-    if (seta.style.display === "" || seta.style.display === "none") {
-        seta.style.display = "none";
+
+    if (ins1.style.display === "" || ins1.style.display === "none") {
+        ins1.style.display = "block";
+        
+        
     } else {
-        seta.style.display = "none";
+        ins1.style.display = "none";
+    }
+
+    
+}
+document.addEventListener('DOMContentLoaded', function () {
+    var edital = document.getElementById("inscricoes2");
+    if (edital) {
+        edital.style.display = "none";  // Initial state: hidden
+    }
+});
+
+function toggletabble_inscricoes2() {
+    var ins1 = document.getElementById("inscricoes1");
+    var ins2 = document.getElementById("inscricoes2");
+    
+
+    if (ins2.style.display === "" || ins2.style.display === "none") {
+        ins2.style.display = "block";
+        ins1.style.display = "none";
+        
+    } else {
+        ins2.style.display = "none";
     }
 }
