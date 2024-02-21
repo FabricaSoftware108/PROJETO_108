bfbc39bb894812d9b31382b3013e3a6793a4d152
jotafreitas <joaocarlofreitas@hotmail.com>
2024-02-21 15:13:21 -0400

    Bootstrap Cabeçalho e Menu Sanduíche
    apaguei o arquivo .js e o que eu coloquei no style, e modifiquei apenas o conteúdo html  do sanduiche.html para bootstrap e já está funcionando

--- js/sanduiche.js
+++
@@ -1,5 +0,0 @@
-function onClickMenuSand() {
-    document.getElementById("menu-sand").classList.toggle("change");
-    document.getElementById("nav-sand").classList.toggle("change-sand");
-    document.getElementById("menu-bg-sand").classList.toggle("change-sand")
-}
