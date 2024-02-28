c109df98e2cfeb2896a42d6e5899c07c4abe20d4
jotafreitas <silvahentony38@gmail.com>
2024-02-28 16:09:59 -0400

    update popup acadastro adms

---
+++ js/cadastro-adm.js
@@ -0,0 +1,8 @@
+let popup = document.getElementById("popup-cadastroAdm")
+
+function openPopup(){
+    popup.classList.add("open-popup")
+}
+function closePopup(){
+    popup.classList.remove("open-popup")
+}
