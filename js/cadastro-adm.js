c6f3f5c90dd1c51921dd70b960c22653bfcea1b7
sackimo <igorloureiro06@gmail.com>
2024-02-28 15:09:29 -0400

    alteração de caminho

---
+++ js/cadastro-adm.js
@@ -0,0 +1,17 @@
+
+
+  $(document).ready(function() {
+    // Função para gerar senha aleatória
+    function generateRandomPassword(length) {
+      var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+      var password = "";
+      for (var i = 0; i < length; i++) {
+        var randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+      }
+      return password;
+    }
+
+    // Atribuir senha aleatória ao campo de senha pré-definida
+    $('#cadastro-adm-senha').val(generateRandomPassword(7));
+  });
