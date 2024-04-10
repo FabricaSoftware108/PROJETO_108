7d0de42f3ca3230d1469f7fd4c8a93e7da6543da
Pedro3DS <pedroca2787@gmail.com>
2024-04-10 15:29:04 -0400

    Update hubfabricasdb.sql

--- mysql/hubfabricasdb.sql
+++ mysql/hubfabricasdb.sql
@@ -18,3 +18,32 @@ create table if not exists editais(
     dataFim date not null,
     arquivo varchar(255) not null
 );
+
+create table if not exists demanda(
+	cnpj varchar(255) not null primary key,
+    nomeEmpresa varchar(255) not null,
+    emailEmpresa varchar(255) not null,
+    estadoEmpresa varchar(255) not null,
+    telefoneEmpresa varchar(255) not null,
+    cidadeEmpresa varchar(255) not null,
+    nomeTitular varchar(255) not null,
+    emailTitular varchar(255) not null,
+    cpf varchar(255) not null,
+    telefoneTitular varchar(255) not null,
+    arquivo varchar(255) not null
+);
+
+create table if not exists vagaEmprego(
+	cnpj varchar(255) not null primary key,
+    nomeEmpresa varchar(255) not null,
+    emailEmpresa varchar(255) not null,
+    cidade varchar(255) not null,
+    telefone varchar(255) not null,
+    estado varchar(255) not null,
+    descricao varchar(10000) not null
+);
+
+create table if not exists bancoTalentos(
+	cpf varchar(255) not null primary key,
+    
+);
