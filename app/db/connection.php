c6b22875552ed8775f0b23b1d3c34be4a87f2edc
12bia <alphawolf244@gmail.com>
2024-04-11 17:16:09 -0400

    Update insert tela de demanda
    Foi colocado os arquivos para inserir dados na tela de demanda ao banco de dados da fábrica

--- app/db/connection.php
+++ app/db/connection.php
@@ -6,3 +6,16 @@ $password = '307108';
 $database = 'hubfabricasdb';
 
 $connection = new mysqli($host, $user, $password, $database);
+
+
+
+
+
+if($connection-> connect_errno){
+    echo "FALHA NA CONEXÃO:(".$connection->connect_errno.")".$connection->connect_error;
+    echo $connection->host_info."\n";/* a função opcional será executada caso a conexão seja bem sucedida.*/
+}
+
+if($connection){
+    echo "conectado com sucesso!";
+}
---
+++ app/session/insert_pagDemanda.php
@@ -0,0 +1,49 @@
+<?php
+include ('../db/connection_demanda.php');
+// include ('verificacao.php');
+
+session_start();
+
+$nome_empresa= mysqli_real_escape_string($connection, $_POST["nome_empresa"]);
+$email_empresa= mysqli_real_escape_string($connection, $_POST["email"]);
+$cnpj= mysqli_real_escape_string($connection, $_POST["cnpj"]);
+$estado= mysqli_real_escape_string($connection, $_POST["estado"]);
+$telefone_empresa= mysqli_real_escape_string($connection, $_POST["telefone"]);
+$cidade= mysqli_real_escape_string($connection, $_POST["cidade"]);
+
+$nome_titular= mysqli_real_escape_string($connection, $_POST["nome"]);
+$email_titular= mysqli_real_escape_string($connection, $_POST["email"]);
+$cpf= mysqli_real_escape_string($connection, $_POST["CPF"]);
+$telefone_titular= mysqli_real_escape_string($connection, $_POST["telefone"]);
+$arquivo= mysqli_real_escape_string($connection, $_POST["arquivo"]);
+
+
+if (empty($nome_empresa) || empty($email_empresa) || empty($cnpj) || empty($estado) || empty($telefone_empresa) 
+|| empty($cidade) || empty($nome_titular) || empty($email_titular) || empty($cpf) || empty($telefone_titular)
+||empty($arquivo)){ /*aqui ele fica retornando para a página de login SE 1 DOS 2 FICAR VAZIO, até a pessoa digitar o login correto.*/
+    header("location: pagDemanda.html");
+    exit();}
+
+$query ="insert into demandas(cnpj, nomeEmpresa,emailEmpresa,estadoEmpresa,telefoneEmpresa,cidadeEmpresa,nomeTitular,emailTitular,cpf,telefoneTitular,arquivo) 
+values ('{$cnpj}', '{$nome_empresa}', '{$email_empresa}','{$estado}','{$telefone_empresa}',
+'{$cidade}','{$nome_titular}','{$email_titular}','{$cpf}','{$telefone_titular}','{$arquivo}')";
+echo $query;
+
+
+$result= mysqli_query($connection,$query);
+echo $result;
+
+if ($result == ''){
+    echo "<script language:'javascript'> window.alert('Não foi possível efetuar o cadastro'); windows.location.href='pagDemanda.html';</script>";
+    
+    }
+    else
+ {
+     echo "<script language:'javascript'> window.alert('Cadastro efetuado com sucesso'); window.location.href='pagDemanda.html';</script>";
+     exit();
+}
+
+
+
+
+?>
