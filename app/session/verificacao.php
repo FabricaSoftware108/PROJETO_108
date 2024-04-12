2023d4410a98a591f39570aacfb96cffacca5414
João Carlo <joaocarlofreitas@hotmail.com>
2024-04-12 14:46:47 -0300

    to do adicionado

--- app/session/verificacao.php
+++ app/session/verificacao.php
@@ -2,8 +2,8 @@
 
 session_start();
 
-if (!$_SESSION['login']){ /* perguntar na daily */
-    header('location: ../../pages/adm/loginAdm.html'); /* perguntar na daily */
+if (!$_SESSION['login']){ /* to do */
+    header('location: ../../pages/adm/loginAdm.html'); 
     exit();
 }
 
