0f0b651ac363cf5d3c690789d55beffb2d1fe30a
jotafreitas <joaocarlofreitas@hotmail.com>
2024-04-12 10:25:02 -0400

    location do header vai pra tela de login

--- app/session/verificacao.php
+++ app/session/verificacao.php
@@ -3,7 +3,7 @@
 session_start();
 
 if (!$_SESSION['login']){ /* perguntar na daily */
-    header('location: index.html'); /* perguntar na daily */
+    header('location: loginAdm.html'); /* perguntar na daily */
     exit();
 }
 
