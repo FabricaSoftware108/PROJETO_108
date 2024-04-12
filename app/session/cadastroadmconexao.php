bc146c0f5fbce9283cf73709977118465e050623
Dudu1705 <eduardoromanoamaral11@gmail.com>
2024-04-12 14:47:37 -0400

    Feature_193
    importei o verificacao,e ja ta conectado com o banco e indo os dados para o servidor

--- app/session/cadastroadmconexao.php
+++ app/session/cadastroadmconexao.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
 include '../db/connection.php';
-// include 'verificacao.php';
+include 'verificacao.php';
 
-
-$senha = mysqli_real_escape_string($connection,$_POST['senha']);
-$nome = mysqli_real_escape_string($connection,$_POST['nome']);
-$email = mysqli_real_escape_string($connection,$_POST['email']);
-$telefone = mysqli_real_escape_string($connection,$_POST['telefone']);
-
+$senha = ($_POST['senha']);
+$nome = ($_POST['nome']);
+$email = ($_POST['email']);
+$telefone = ($_POST['telefone']);
 
 if(empty($nome) || empty($email)){
     echo "<script>alert('Insira os dados corretamente');</script>";
@@ -16,20 +14,14 @@ if(empty($nome) || empty($email)){
     exit();
 }
 
-
-$query = "INSERT INTO admin (email, senha, nome, telefone, estado) VALUES ('$email', '$senha', '$nome', '$telefone')";
+$query = "INSERT INTO admin (email, senha, nome, telefone, estado) VALUES ('$email', '$senha', '$nome', '$telefone', true)";
 $result = mysqli_query($connection, $query);
-$row=mysqli_num_rows($result);
 
-
-if ($row==0){
-    echo "<script>alert('ERRO AO CADASTRAR');</script>";
-    header("location:../../../pages/adm/cadastroAdm.html");
-    exit();
-}
-
-else if($row>0){
+if ($result){
     echo "<script>alert('CADASTRADO COM SUCESSO');</script>";
+    header("location:../../../pages/adm/cadastroAdm.html");
+} else {
+    echo "<script>alert('ERRO AO CADASTRAR');</script>";
     header("location:../../../pages/adm/cadastroAdm.html");
 }
 ?>
