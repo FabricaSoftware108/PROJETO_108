2cd0f847292bcf507abccf2e0173baa8cb0f989b
Gleison <gleisonmorais06@gmail.com>
2024-04-12 14:50:33 -0400

    Feat Implement  bd to vagasEmprego table
    diz a implementacao com banco de dados no vagasEmprego

--- app/session/conexaoVagasEmprego.php
+++ app/session/conexaoVagasEmprego.php
@@ -1,19 +1,26 @@
 <?php
 
-include '.php'; // Vo precisar do banco agora
-
-if(isset($_POST['nome_empresa']) && isset($_POST['email']) && isset($_POST['cnpj']) && isset($_POST['cidade']) && isset($_POST['telefone']) && isset($_POST['estado']) && isset($_POST['campoTextoDescricaoVaga'])){
+include '../db/connection.php';
+include
+if (isset($_POST['nome_empresa']) && 
+    isset($_POST['email']) && 
+    isset($_POST['cnpj']) && 
+    isset($_POST['cidade']) && 
+    isset($_POST['telefone']) && 
+    isset($_POST['estado']) && 
+    isset($_POST['descricao_vaga'])){
+        
     $nome_empresa = $_POST['nome_empresa'];
     $email = $_POST['email'];
     $cnpj = $_POST['cnpj'];
     $cidade = $_POST['cidade'];
     $telefone = $_POST['telefone'];
     $estado = $_POST['estado'];
-    $descricao_vaga = $_POST['campoTextoDescricaoVaga'];
+    $descricao_vaga = $_POST['descricao_vaga'];
 
-    $query = "INSERT INTO cadastro_vagas (nome_empresa, email, cnpj, cidade, telefone, estado, descricao_vaga) VALUES ('$nome_empresa', '$email', '$cnpj', '$cidade', '$telefone', '$estado', '$descricao_vaga')";
+    $query = "INSERT INTO vagasEmprego (cnpj, nomeEmpresa, emailEmpresa, cidade, telefone, estado, descricao) VALUES ('$cnpj','$nome_empresa', '$email', '$cidade', '$telefone', '$estado', '$descricao_vaga')";
     
-    $result = mysqli_query($con, $query);
+    $result = mysqli_query($connection, $query);
     
 } else {
     header("location: ../../pages/editais/cadastroVagasEmprego.html");
