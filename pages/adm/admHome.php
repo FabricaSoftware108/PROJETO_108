d0bd2cfae56793df933ca7a74e738a85c245756a
Vinicius <vinicius2002mr@gmail.com>
2024-04-17 14:37:36 -0400

    adicionando o código de gráfico
    adicionando o código de gráfico

--- pages/adm/admHome.php
+++ pages/adm/admHome.php
@@ -129,7 +129,47 @@ include '../../app/session/verificacao.php';
     <div class="container w-100" id="container2AdmHomeImagem">
   <div class="row justify-content-center d-flex align-items-center">
     <div class="col-sm-12 col-lg-8">
-      <img src="../../Imagens/imagem_grafico_adm_home.png" class="img-fluid" alt="Responsive image" id="adm-home-img-2">
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+    google.charts.load("current", {packages:['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ["Element", "Density", { role: "style" } ],
+        ["Copper", 8.94, "#b87333"],
+        ["Silver", 10.49, "silver"],
+        ["Gold", 19.30, "gold"],
+        ["Platinum", 21.45, "color: #e5e4e2"]
+      ]);
+
+      var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "Density of Precious Metals, in g/cm^3",
+        width: 600,
+        height: 400,
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+      chart.draw(view, options);
+  }
+  </script>
+  
+<div id="columnchart_values" style="width: 900px; height: 300px;"></div>
+<!-- Se quiséssemos formatar o valor de maneira diferente, poderíamos definir um formatador e uni-lo em uma função como esta:
+
+
+      function getValueAt(column, dataTable, row) {
+        return dataTable.getFormattedValue(row, column);
+      } -->
     </div>
   </div>
 </div>
