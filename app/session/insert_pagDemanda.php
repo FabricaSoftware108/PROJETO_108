b0ec66ada3b7721ef44f3b6f79b3d37168c0d943
12bia <alphawolf244@gmail.com>
2024-04-17 16:14:59 -0400

    Update tela de demanda

--- app/session/insert_pagDemanda.php
+++ app/session/insert_pagDemanda.php
@@ -1,6 +1,6 @@
 <?php
-include ('../db/connection.php');
-include ('verificacao.php');
+include ('conexao_demanda.php');
+// include ('verificacao.php');
 
 session_start();
 
@@ -12,9 +12,9 @@ $telefone_empresa= mysqli_real_escape_string($connection, $_POST["telefone"]);
 $cidade= mysqli_real_escape_string($connection, $_POST["cidade"]);
 
 $nome_titular= mysqli_real_escape_string($connection, $_POST["nome"]);
-$email_titular= mysqli_real_escape_string($connection, $_POST["email_titular"]);
+$email_titular= mysqli_real_escape_string($connection, $_POST["email"]);
 $cpf= mysqli_real_escape_string($connection, $_POST["CPF"]);
-$telefone_titular= mysqli_real_escape_string($connection, $_POST["telefone_titular"]);
+$telefone_titular= mysqli_real_escape_string($connection, $_POST["telefone"]);
 $arquivo= mysqli_real_escape_string($connection, $_POST["arquivo"]);
 
 
@@ -22,6 +22,7 @@ if (empty($nome_empresa) || empty($email_empresa) || empty($cnpj) || empty($esta
 || empty($cidade) || empty($nome_titular) || empty($email_titular) || empty($cpf) || empty($telefone_titular)
 ||empty($arquivo)){ /*aqui ele fica retornando para a página de login SE 1 DOS 2 FICAR VAZIO, até a pessoa digitar o login correto.*/
     header("location: pagDemanda.html");
+//script Informa campos vazios
     exit();}
 
 $query ="insert into demandas(cnpj, nomeEmpresa,emailEmpresa,estadoEmpresa,telefoneEmpresa,cidadeEmpresa,nomeTitular,emailTitular,cpf,telefoneTitular,arquivo) 
@@ -43,5 +44,56 @@ if ($result == ''){
      exit();
 }
 
+
+/* essas linhas é que estavam dando erro, e não estavam deixando inserir.*/
+// $retorno = mysqli_fetch_array($result);
+// echo $result;
+
+
+
+
+// echo $row;
+
+
+
+//----------------------------------------------------------------------------------------------------------//
+
+/*AQUI SE O ROW RETORNAR MAIOR QUE 0 ELE VAI  PARA A TELA SEGUINTE*/
+// if($row>0){
+//     $_SESSION["nome"]=$nome;
+//     $_SESSION["email"]=$retorno["email"];
+//     header("location:pagDemanda.html");
+// }
+
+    // if($_SESSION["email"]=="admin"){
+    // header("location:admin.php");}
+       
+   
+    //  else if($_SESSION["setor"]=="user"){
+    //      header("location:page.html");
+    //  }
+    //  exit();
+    //  }
+
+     
+    //  esse daqui serve para pesquisar certinho por que não entendi direito.
+    //  if(!$_SESSION["setor"]=="admin"){
+    //     header("location:index.html");}
+
+    // if(!$_SESSION["setor"]=="user"){
+    //     header("location:admin.php");}
+
+
+  
+ 
+//   else if($row==0){
+//      header("location:pagDemanda.html"); /*AQUI SE O ROW RETORNAR O VALOR IGUAL A 0 ELE FICA NESSE LOPPING INFINITO DA TELA DE LOGIN*/
+//      exit();
+//   }
+
+// //  o setor serve para fazer a atribuição de alguma página,
+// //  ao cargo expecifico, exemplo: usuarios do setor admin,
+// //  serao direcionados as paginas de 
+// //  admin.php já usuarios comuns serao redirecionados para as paginas de usuario
 
 ?>
