aee789d0c602457be92dbfdde7917a6f88cc91c9
Larissa <larissacgr05@gmail.com>
2024-04-18 18:39:33 -0300

    update cadastroAlunoPsg

--- app/session/conexaoCadastroAlunoPsg.php
+++ app/session/conexaoCadastroAlunoPsg.php
@@ -3,33 +3,31 @@ session_start();
 include '../db/connection.php';
 
 #Dados Pessoais
-$nomeAluno = mysqli_real_escape_string($con,$_POST['nomeAluno']);
-$cpfAluno = mysqli_real_escape_string($con,$_POST['CPFaluno']);
-$rgAluno = mysqli_real_escape_String($con,$_POST['RGaluno']);
-$emailAluno = mysqli_real_escape_string($con,$_POST['emailAluno']);
-$telefoneAluno = mysqli_real_escape_string($con,$_POST['TelefoneAluno']);
-$nomePai = mysqli_real_escape_String($con,$_POST['NomePai']);
-$nomeMae = mysqli_real_escape_String($con,$_POST['NomeMãe']);
+$nomeAluno = ($_POST['nome']);
+$cpfAluno = ($_POST['cpf']);
+$rgAluno = ($_POST['rg']);
+$emailAluno = ($_POST['email']);
+$telefoneAluno = ($_POST['telefone']);
+$nomePai = ($_POST['nomePai']);
+$nomeMae = ($_POST['nomeMae']);
 
 #Endereço
-$ruaAluno = mysqli_real_escape_String($con,$_POST['Rua']);
-$bairroAluno = mysqli_real_escape_String($con,$_POST['Bairro']);
-$numeroEnderecoAluno = mysqli_real_escape_String($con,$_POST['Número']);
-$cepAluno = mysqli_real_escape_String($con,$_POST['Cep']);
-$complementoAluno = mysqli_real_escape_String($con,$_POST['Complemento']);
+$ruaAluno = ($_POST['rua']);
+$bairroAluno = ($_POST['bairro']);
+$numeroEnderecoAluno = ($_POST['numero']);
+$cepAluno = ($_POST['cep']);
+$complementoAluno = ($_POST['complemento']);
 
 // dar um window alert para orientar o aluno
-if(empty($nome) || empty($cpfAluno) || empty($rgAluno) || empty($telefoneAluno) || empty($emailAluno) || empty($nomeMae) || empty($nomePai) || empty($ruaAluno) || empty($bairroAluno)|| empty($numeroEnderecoAluno) || empty($cepAluno) || empty($complementoAluno)){
+if(empty($nomeAluno) || empty($cpfAluno) || empty($rgAluno) || empty($telefoneAluno) || empty($emailAluno) || empty($nomeMae) || empty($nomePai) || empty($ruaAluno) || empty($bairroAluno)|| empty($numeroEnderecoAluno) || empty($cepAluno) || empty($complementoAluno)){
     header("location:../../pages/editais/cadastroAlunoPsg.php");
     exit();
 }
 
-$query = "INSERT INTO alunosPsg (cpf, nome, rg, telefone, email, nomeMae, nomePai, rua, bairro, numero, cep, complemento) 
 
-VALUES ('$nomeAluno','$cpfAluno','$rgAluno','$emailAluno','$telefoneAluno','$nomePai','$nomeMae',
-'$ruaAluno','$bairroAluno','$numeroEnderecoAluno','$cepAluno','$complementoAluno')";
+$query = "INSERT INTO alunosPsg () VALUES (null,'$cpfAluno','$nomeAluno','$rgAluno','$telefoneAluno','$emailAluno','$nomePai','$nomeMae','$ruaAluno','$bairroAluno','$numeroEnderecoAluno','$complementoAluno','$cepAluno','50/2024');";
 
-$result = mysqli_query($con,$query);
+$result = mysqli_query($connection, $query);
 
 
 if ($result){
