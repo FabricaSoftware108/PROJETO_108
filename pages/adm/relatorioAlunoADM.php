e5799e3392182e40a176e41e3edb58c640051a3d
Diego <dih.yokoyama03@gmail.com>
2024-04-18 16:19:11 -0400

    direcionamento de paginas
    editar carrossel e cadastro carrossel estão direcionando para os locais corretos.

--- pages/adm/relatorioAlunoADM.php
+++ pages/adm/relatorioAlunoADM.php
@@ -61,7 +61,7 @@ include '../../app/session/verificacao.php';
                     Fábricas
                   </a>
                   <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="./cadastroCarrossel.php">Cadastrar Carrossel</a></li>
+                    <li><a class="dropdown-item" href="./adicionarCarrosselAdm.php">Cadastrar Carrossel</a></li>
                     <li><a class="dropdown-item" href="./editarCarrossel.php">Editar Carrossel </a></li>
                  </ul>
                 </li>
