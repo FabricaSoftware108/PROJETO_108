7bfbfe6a971bd82ad8528cffdbcc558d1f058cd5
Larissa <larissacgr05@gmail.com>
2024-04-22 17:30:11 -0300

    troca de html para php, add php

--- pages/adm/esqueceuSenhaAdm.php
+++ pages/adm/esqueceuSenhaAdm.php
@@ -14,9 +14,31 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </head>
+        
+        <?php
+         session_start();
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+                $email = $_POST ['email'];
+                $novaSenha = substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(8/strlen($x)) )),1,8);
+
+                $to = $email;
+                $subject = 'Recuperação de Senha';
+                $message = 'Sua nova senha é: ' . $novaSenha;
+                $headers = 'From: larissacgr05@gmail.com';
+
+                if (mail($to, $subject, $message, $headers)) {
+                    echo 'E-mail de recuperação de senha enviado com sucesso.';
+                } else {
+                    echo 'Erro ao enviar o e-mail de recuperação de senha.';
+                }
+            }
+        ?>
+    </head> 
+
 
     <body class="pag-login-ADM">
+    
         <a href="./loginAdm.html"><svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-arrow-left-circle-fill mx-2 my-4" viewBox="0 0 16 16">
             <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
           </svg></a>
