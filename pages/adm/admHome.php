3d0b28422713a76b7c76662927f03224d75a8159
evandro-ssilva <evandro_07@outlook.com>
2024-04-23 14:40:31 -0400

    atualizando o codigo
    fonte dos numeros agora está legivel

--- pages/adm/admHome.php
+++ pages/adm/admHome.php
@@ -26,126 +26,129 @@ $retornoBT = mysqli_num_rows($dadosBT);
 
 
   <!-- Link Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous"></script>
- 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 
   <!-- CSS -->
   <link href="../../css/styles.css" rel="stylesheet">
   <!--Menu de Navegação-->
 
 
-        <!-- Fonte -->
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
-    </head>
-    <body class="body-adm-home">
+  <!-- Fonte -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+</head>
 
-        <!--Menu de Navegação-->
-        <header> 
+<body class="body-adm-home">
+
+  <!--Menu de Navegação-->
+  <header>
     <nav class="navbar navbar-dark bg-primary fixed-top nav-adm">
-        <div class="container-fluid">
-            <div class="md-auto">
+      <div class="container-fluid">
+        <div class="md-auto">
           <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-            </div>
-            <a class="navbar-brand me-auto fs-2 fw-bold" id="titulo-cabecalho-adm" href="#">Área de Administração Hub Fábricas</a>
-            <a class="navbar-brand md-auto fs-6" id="nome-cabecalho-adm" href="#"><?php echo $_SESSION['nome']?></a>
-          <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-            <div class="offcanvas-header ">
-              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"></h5><?php echo $_SESSION['nome']?>
-              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-              <ul class="navbar-nav flex-column pe-3">
-                <li class="nav-item">
-                  <a class="nav-link active fs-5" aria-current="page" href="./admHome.php">Home</a>
-                </li>
-                <li class="nav-item dropdown-adm">
-                  <a class="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Administradores
-                  </a>
-                  <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="./cadastroAdm.php">Adicionar Administradores</a></li>
-                    <li><a class="dropdown-item" href="./editarAdm.php">Editar Administradores </a></li>
-                 </ul>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link fs-5" aria-current="page" href="./cadastroAdmEdital.php">Editais</a>
-                </li>
-
-                <li class="nav-item dropdown-adm">
-                  <a class="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Fábricas
-                  </a>
-                  <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="./adicionarCarrosselAdm.php">Cadastrar Carrossel</a></li>
-                    <li><a class="dropdown-item" href="./editarCarrosselAdm.php">Editar Carrossel </a></li>
-                 </ul>
-                </li>
-
-
-                <li class="nav-item dropdown-adm">
-                  <a class="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Relatórios
+        </div>
+        <a class="navbar-brand me-auto fs-2 fw-bold" id="titulo-cabecalho-adm" href="#">Área de Administração Hub Fábricas</a>
+        <a class="navbar-brand md-auto fs-6" id="nome-cabecalho-adm" href="#"><?php echo $_SESSION['nome'] ?></a>
+        <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div class="offcanvas-header ">
+            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"></h5><?php echo $_SESSION['nome'] ?>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav flex-column pe-3">
+              <li class="nav-item">
+                <a class="nav-link active fs-5" aria-current="page" href="./admHome.php">Home</a>
+              </li>
+              <li class="nav-item dropdown-adm">
+                <a class="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Administradores
                 </a>
                 <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="./relatorioAlunoADM.php">Edital Aluno</a></li>
-                    <li><a class="dropdown-item" href="./relatorioeditaldaempresa.php">Edital Empresa</a></li>
-                    <li><a class="dropdown-item" href="./relatoriobancotalento.php">Banco de Talentos</a></li>
-                    <li><a class="dropdown-item" href="./relatorioVagasEmprego.php">Vagas</a></li>
+                  <li><a class="dropdown-item" href="./cadastroAdm.php">Adicionar Administradores</a></li>
+                  <li><a class="dropdown-item" href="./editarAdm.php">Editar Administradores </a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fs-5" aria-current="page" href="./cadastroAdmEdital.php">Editais</a>
+              </li>
+
+              <li class="nav-item dropdown-adm">
+                <a class="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Fábricas
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark">
+                  <li><a class="dropdown-item" href="./adicionarCarrosselAdm.php">Cadastrar Carrossel</a></li>
+                  <li><a class="dropdown-item" href="./editarCarrosselAdm.php">Editar Carrossel </a></li>
+                </ul>
+              </li>
+
+
+              <li class="nav-item dropdown-adm">
+                <a class="nav-link dropdown-toggle fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Relatórios
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark">
+                  <li><a class="dropdown-item" href="./relatorioAlunoADM.php">Edital Aluno</a></li>
+                  <li><a class="dropdown-item" href="./relatorioeditaldaempresa.php">Edital Empresa</a></li>
+                  <li><a class="dropdown-item" href="./relatoriobancotalento.php">Banco de Talentos</a></li>
+                  <li><a class="dropdown-item" href="./relatorioVagasEmprego.php">Vagas</a></li>
 
                 </ul>
-                </li>
+              </li>
 
 
-                <li class="nav-item fs-5">
-                  <a class="nav-link" href="./usuarioAdm.php">Usuário</a>
-                </li>
-                <li class="nav-item mt-auto mb-2">
-                  <a class="nav-link fs-5" href="../../app/session/logout.php">Sair</a>
-                </li>
-              </ul>
-            </div>
+              <li class="nav-item fs-5">
+                <a class="nav-link" href="./usuarioAdm.php">Usuário</a>
+              </li>
+              <li class="nav-item mt-auto mb-2">
+                <a class="nav-link fs-5" href="../../app/session/logout.php">Sair</a>
+              </li>
+            </ul>
           </div>
         </div>
-      </nav>        
+      </div>
+    </nav>
   </header>
 
-        <!--Conteúdo da página-->
-        <main class="main-adm-home">
-          <div class="container" id="container-1-adm-home">
-            <div class="row justify-content-center" id="row-1-adm-home">
-              <div class="col-sm-12 col-lg-8" id="label-bem-vindos-adm-home">
-                <b><p class="text-center">BEM VINDO!!</p></b>  
-              </div>
-            </div>
+  <!--Conteúdo da página-->
+  <main class="main-adm-home">
+    <div class="container" id="container-1-adm-home">
+      <div class="row justify-content-center" id="row-1-adm-home">
+        <div class="col-sm-12 col-lg-8" id="label-bem-vindos-adm-home">
+          <b>
+            <p class="text-center">BEM VINDO!!</p>
+          </b>
+        </div>
+      </div>
 
       <!--Quadrados com as informações-->
       <div class="row justify-content-center" id="row-2-adm-home">
-        <div class="col-sm-12 col-lg-3 w-sm-100" id="label-quadrados-1-adm-home">
-          <p class="text-center"><p class="numero-adm-home"><?php echo$retornoDemandas?></p><br>Projetos Submetidos</p>
+        <div class="col-sm-12 col-lg-3 w-sm-100 shadow-lg" id="label-quadrados-1-adm-home">
+          <p class="text-center">
+          <p class="numero-adm-home"><?php echo $retornoDemandas ?></p><br>Projetos Submetidos</p>
         </div>
-        <div class="col-sm-12 col-lg-3 offset-lg-1 w-sm-100" id="label-quadrados-2-adm-home">
-          <p class="text-center"><p class="numero-adm-home"><?php echo$retornoVagas?></p><br>Inscrição - Vagas de Emprego</p>
+        <div class="col-sm-12 col-lg-3 offset-lg-1 w-sm-100 shadow-lg" id="label-quadrados-2-adm-home">
+          <p class="text-center">
+          <p class="numero-adm-home"><?php echo $retornoVagas ?></p><br>Inscrição - Vagas de Emprego</p>
         </div>
-        <div class="col-sm-12 col-lg-3 offset-lg-1 w-sm-100" id="label-quadrados-3-adm-home">
-          <p class="text-center"><p class="numero-adm-home"><?php echo$retornoBT?></p><br>Banco de Talentos</p>
+        <div class="col-sm-12 col-lg-3 offset-lg-1 w-sm-100 shadow-lg" id="label-quadrados-3-adm-home">
+          <p class="text-center">
+          <p class="numero-adm-home"><?php echo $retornoBT ?></p><br>Banco de Talentos</p>
         </div>
       </div>
     </div>
 
     <!--Imagem dos gráficos-->
     <div class="container w-100" id="container2AdmHomeImagem">
-  <div class="row justify-content-center d-flex align-items-center">
-    <div class="col-sm-12 col-lg-8">
-      <img src="../../Imagens/imagem_grafico_adm_home.png" class="img-fluid" alt="Responsive image" id="adm-home-img-2">
+      <div class="row justify-content-center d-flex align-items-center">
+        <div class="col-sm-12 col-lg-8">
+          <img src="../../Imagens/imagem_grafico_adm_home.png" class="img-fluid" alt="Responsive image" id="adm-home-img-2">
+        </div>
+      </div>
     </div>
-  </div>
-</div>
 
   </main>
 
