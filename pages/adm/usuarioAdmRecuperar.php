d15438c76d54ee1d862efe5e98b308c0f57a33fb
Diego <dih.yokoyama03@gmail.com>
2024-04-23 17:27:44 -0400

    mudei o title so usuario adm

--- pages/adm/usuarioAdmRecuperar.php
+++ pages/adm/usuarioAdmRecuperar.php
@@ -7,7 +7,7 @@ include '../../app/session/verificacao.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Recuperar Senha ADM</title>
+  <title>Alterar Senha ADM</title>
   <link rel="stylesheet" href="../../css/styles.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
