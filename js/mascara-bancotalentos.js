8d69c0ceb3e204a36e39a966be197afef0c515a2
Niicolas <nicollasrubini@gmail.com>
2024-05-02 16:41:16 -0400

    mascaras adicionadas

--- js/mascara-bancotalentos.js
+++ js/mascara-bancotalentos.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-    $('#cnpj').on('input', function() {
-        var cnpj = $(this).val().replace(/\D/g, '');
-        if (cnpj.length > 0) {
-            cnpj = cnpj.replace(/^(\d{2})(\d)/, '$1.$2');
-            cnpj = cnpj.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
-            cnpj = cnpj.replace(/\.(\d{3})(\d)/, '.$1/$2');
-            cnpj = cnpj.replace(/(\d{4})(\d)/, '$1-$2');
-        }
-        $(this).val(cnpj);
-    });
-
     $('#telefone').on('input', function() {
         var telefone = $(this).val().replace(/\D/g, '');
         if (telefone.length > 0) {
@@ -19,7 +8,7 @@ $(document).ready(function(){
         $(this).val(telefone);
     });
 
-    $('#CPF').on('input', function() {
+    $('#cpf').on('input', function() {
         var cpf = $(this).val().replace(/\D/g, '');
         if (cpf.length > 0) {
             cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2');
