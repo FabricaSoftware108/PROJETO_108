42ea8600a54909d99e508fc26ef583bf49847d50
Gabriel Gravena Barros <132004805+oppsggbarros@users.noreply.github.com>
2024-05-02 14:20:00 -0300

    novo switch

--- pages/adm/editarAdm.php
+++ pages/adm/editarAdm.php
@@ -13,14 +13,13 @@ $result = mysqli_query($connection, $query);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Editar Admin</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="../../css/styles.css">
   <script src="../../js/cadastro-adm.js"></script>
+  <link rel="stylesheet" type="text/css" href="//assets.locaweb.com.br/locastyle/3.10.1/stylesheets/locastyle.css">
+
 
 </head>
 
@@ -35,10 +34,10 @@ $result = mysqli_query($connection, $query);
           </button>
             </div>
             <a class="navbar-brand me-auto fs-2 fw-bold" id="titulo-cabecalho-adm" href="#">Área de Administração Hub Fábricas</a>
-            <a class="navbar-brand md-auto fs-6" id="nome-cabecalho-adm" href="#"><?php echo $_SESSION['nome']?></a>
+            <a class="navbar-brand md-auto fs-6" id="nome-cabecalho-adm" href="#"><?php echo $_SESSION['nome'] ?></a>
           <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
             <div class="offcanvas-header ">
-              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"></h5><?php echo $_SESSION['nome']?>
+              <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"></h5><?php echo $_SESSION['nome'] ?>
               <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
             </div>
             <div class="offcanvas-body">
@@ -97,42 +96,45 @@ $result = mysqli_query($connection, $query);
       </nav>        
   </header> -->
 
-  <main class="wrapper-editar-adm editar-adm" >
-      <div class="container-fluid">
-        <div class="row justify-content-center">
-          <div class="col-md-5 col-11 justify-content-center align-items-center container-editar-adm" >
-            <div class="col-12 editar-adm-container-title text-center" >
-              <h1>Administradores</h1>
-            </div>
-            <div class="container-fluid">
-            <?php while($row = mysqli_fetch_array($result)){ ?>
+  <main class="wrapper-editar-adm editar-adm">
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-md-5 col-11 justify-content-center align-items-center container-editar-adm">
+          <div class="col-12 editar-adm-container-title text-center">
+            <h1>Administradores</h1>
+          </div>
+          <div class="container-fluid">
+            <?php while ($row = mysqli_fetch_array($result)) { ?>
               <div class="row p-0">
-                <div class="col-10 container-name-adm text-center" style="overflow-x: hidden;">
-                  <div class="admin-entry mb-3 p-2 border " >
+                <div class="col-12 container-name-adm text-center" style="overflow-x: hidden;">
+                  
                     <div class="row">
-                    <div class="col-md-6 col-12" >
-                      <p><?php echo $row['nome'] ?></p>
+                      <div class="col-md-12 col-12">
+                       
+                        <div class="ls-box">
+                          <h2 class="ls-title-5 ls-display-inline-block"><?php echo $row['nome'] ?></h2>
+                          <h2 class="ls-title-5 ls-display-inline-block"><?php echo $row['email'] ?></h2>
+
+                          <div data-ls-module="switchButton" class="ls-switch-btn ls-float-right">
+                            <input type="checkbox" id="teste">
+                            <label class="ls-switch-label" for="teste" name="label-teste" ><span></span></label>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <div class="col-md-6 col-12">
-                      <p><?php echo $row['email'] ?></p>
-                    </div>
-                  </div>
+                  
                 </div>
+
               </div>
-              <div class="col-2 justify-content-center align-items-center d-flex switch-active-adm m-0">
-                <label class="editar-adm-switch">
-                  <input type="checkbox" class="cadastro-adm-checkbox">
-                  <span class="editar-adm-slider"></span>
-                  <span class="editar-adm-knob"></span>
-                </label>  
-              </div>
-            </div>
             <?php } ?>
-            </div>
           </div>
         </div>
       </div>
+    </div>
   </main>
+  <footer>
+  <script src="//assets.locaweb.com.br/locastyle/3.10.1/javascripts/locastyle.js"></script> 
+  </footer>
 
 
   <script src="../js/cadastro-adm.js"></script>
