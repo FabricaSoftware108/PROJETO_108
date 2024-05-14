ba430e8c2b319fba91621e0e7fb9c8831b922cd1
12bia <alphawolf244@gmail.com>
2024-05-14 17:15:07 -0400

    Update verificaco de cpf e cnpj.js

--- js/validacao-cpf-cnpj.js
+++ js/validacao-cpf-cnpj.js
@@ -2,59 +2,66 @@
 // var cpf = "169.169.000-75";
 //    cpf = "085.430.241-71";
 
-var cpf= document.getElementById('CPF');
 
-function isCPF(cpf=0){
-    console.log(cpf);
-    cpf = cpf.replace(/\.|-/g,"");
+var cpf= document.getElementsByClassName('cpfverification');
 
-    let soma = 0;
-    soma += cpf[0] * 10;
-    soma += cpf[1] * 9;
-    soma += cpf[2] * 8;
-    soma += cpf[3] * 7;
-    soma += cpf[4] * 6;
-    soma += cpf[5] * 5;
-    soma += cpf[6] * 4;
-    soma += cpf[7] * 3;
-    soma += cpf[8] * 2;
-    soma = (soma * 10 ) % 11;
+document.querySelector("#CPF").addEventListener("input",function(e){
 
-    if(soma == 10 || soma == 11)
-        soma = 0;
-
-    console.log("Primeiro digito: "+soma);
-
-    if(soma != cpf[9] )
-        return false;
-
-    soma = 0;
-    soma += cpf[0] * 11;
-    soma += cpf[1] * 10;
-    soma += cpf[2] * 9;
-    soma += cpf[3] * 8;
-    soma += cpf[4] * 7;
-    soma += cpf[5] * 6;
-    soma += cpf[6] * 5;
-    soma += cpf[7] * 4;
-    soma += cpf[8] * 3;
-    soma += cpf[9] * 2;
-    soma = (soma * 10 ) % 11;
-
-    if(soma == 10 || soma == 11)
-        soma = 0;
-
+    function isCPF(cpf=0){
+        alert(cpf);
+        cpf = cpf.replace(/\.|-/g,"");
     
+        let soma = 0;
+        soma += cpf[0] * 10;
+        soma += cpf[1] * 9;
+        soma += cpf[2] * 8;
+        soma += cpf[3] * 7;
+        soma += cpf[4] * 6;
+        soma += cpf[5] * 5;
+        soma += cpf[6] * 4;
+        soma += cpf[7] * 3;
+        soma += cpf[8] * 2;
+        soma = (soma * 10 ) % 11;
+    
+        if(soma == 10 || soma == 11)
+            soma = 0;
+    
+        console.log("Primeiro digito: "+soma);
+    
+        if(soma != cpf[9] )
+            return false;
+    
+        soma = 0;
+        soma += cpf[0] * 11;
+        soma += cpf[1] * 10;
+        soma += cpf[2] * 9;
+        soma += cpf[3] * 8;
+        soma += cpf[4] * 7;
+        soma += cpf[5] * 6;
+        soma += cpf[6] * 5;
+        soma += cpf[7] * 4;
+        soma += cpf[8] * 3;
+        soma += cpf[9] * 2;
+        soma = (soma * 10 ) % 11;
+    
+        if(soma == 10 || soma == 11)
+            soma = 0;
+    
+        
+    
+        if(soma != cpf[10] )
+            return false;
+    
+    
+        console.log("Segundo digito: "+soma);
+        return true;
+    }
+    console.log(isCPF(cpf));
+    
+    document.getElementById("#CPF").addEventListener("input", function (e){
+        console.log(e.target.value)
+    })
 
-    if(soma != cpf[10] )
-        return false;
-
-
-    console.log("Segundo digito: "+soma);
-    return true;
-}
-console.log(isCPF(cpf));
-
-document.querySelector("#CPF").addEventListener("input", function (e){
-    console.log(e.target.value)
 })
+
+
