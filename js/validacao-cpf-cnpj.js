c6de331c62ded05e428187e867a2de4896dd3461
12bia <alphawolf244@gmail.com>
2024-05-16 16:16:30 -0400

    Update verificacao de cpf e cnpj com js

--- js/validacao-cpf-cnpj.js
+++ js/validacao-cpf-cnpj.js
@@ -1,63 +1,57 @@
+// aqui ele pega o valor que tem no meu input.
+// const form = document.querySelector('#campo-demanda')
+// const campo = document.querySelector('.inputCPF');
 
-const form = document.querySelector('#campo-demanda')
-const campo = document.querySelector('.inputCPF');
+function verificarCPF(strCpf) {
+    if (!/[0-9]{11}/.test(strCpf)) return false;
+    if (strCpf === "00000000000") return false;
 
-form.addEventListener('submit',function(event){
-    
-    const input = campo.value
-    console.log(input)
+    var soma = 0;
 
-    cpf = input.replace(/\.|-/g,"");// aqui tira todos os caracteres e deixa apenas os numeros.
-        let soma = 0;
-        soma += cpf[0] * 10;
-        soma += cpf[1] * 9;
-        soma += cpf[2] * 8;
-        soma += cpf[3] * 7;
-        soma += cpf[4] * 6;
-        soma += cpf[5] * 5;
-        soma += cpf[6] * 4;
-        soma += cpf[7] * 3;
-        soma += cpf[8] * 2;
-        soma = (soma * 10 ) % 11;
-    
-        if(soma == 10 || soma == 11)
-            soma = 0;
-    
-        console.log("Primeiro digito: "+soma);
-    
-        if(soma != cpf[9] )
-            return false;
-        //   alert("DIGITE O CPF CERTO")
-    
-        soma = 0;
-        soma += cpf[0] * 11;
-        soma += cpf[1] * 10;
-        soma += cpf[2] * 9;
-        soma += cpf[3] * 8;
-        soma += cpf[4] * 7;
-        soma += cpf[5] * 6;
-        soma += cpf[6] * 5;
-        soma += cpf[7] * 4;
-        soma += cpf[8] * 3;
-        soma += cpf[9] * 2;
-        soma = (soma * 10 ) % 11;
-    
-        if(soma == 10 || soma == 11)
-            soma = 0;
-    
-        
-    
-        if(soma != cpf[10] )
-            return false;
-        // alert("DIGITE O CPF CERTO")
-    
-    
-        console.log("Segundo digito: "+soma);
-        return true;
-
-    
+    for (var i = 1; i <= 9; i++) {
+        soma += parseInt(strCpf.substring(i - 1, i)) * (11 - i);
     }
-    
 
-)
+    var resto = soma % 11;
+
+    if (resto === 10 || resto === 11 || resto < 2) {
+        resto = 0;
+    } else {
+        resto = 11 - resto;
+    }
+
+    if (resto !== parseInt(strCpf.substring(9, 10))) {
+        return false;
+    }
+
+    soma = 0;
+
+    for (var i = 1; i <= 10; i++) {
+        soma += parseInt(strCpf.substring(i - 1, i)) * (12 - i);
+    }
+    resto = soma % 11;
+
+    if (resto === 10 || resto === 11 || resto < 2) {
+        resto = 0;
+    } else {
+        resto = 11 - resto;
+    }
+
+    if (resto !== parseInt(strCpf.substring(10, 11))) {
+        return false;
+    }
+
+    return true;
+}
+
+function validarNome() {
+    var strCpf = document.getElementById('CPF').value;
+    if (!verificarCPF(strCpf)) {
+        alert("CPF inválido");
+        return;
+    }
+    document.getElementById('campo-demanda').submit();
+}
   
+
+
