28a1b4bfbcc71d03c4db24b50d4270f92a7823f4
Gleison <gleisonmorais06@gmail.com>
2024-05-16 17:07:02 -0400

    Feat Implement JS and Animated rows factories

---
+++ js/effect-home-factories.js
@@ -0,0 +1,10 @@
+const rowSoftware = document.getElementById('animated-left-js-software');
+const rowGame = document.getElementById('animated-right-js-game');
+const rowGrafico = document.getElementById('animated-left-js-grafica');
+const rowAudio = document.getElementById('animated-right-js-audio');
+
+// rowSoftware.style.display = 'none';
+
+// rowGame.style.display = 'block'; 
+// rowGrafico.style.display = 'block';
+// rowAudio.style.display = 'block';
