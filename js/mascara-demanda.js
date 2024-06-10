88ccba464482a90374ec2bf1c6dbd444a6d83a5e
Diego <dih.yokoyama03@gmail.com>
2024-06-10 15:58:41 -0400

    modifiquei os nomes dos input e o titulo
    modifiquei os nomes dos input e o titulo

--- js/mascara-demanda.js
+++ js/mascara-demanda.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
         $(this).val(cnpj);
     });
 
-    $('#telefone_empresa').on('input', function() {
+    $('#telefone_Pessoal/empresa').on('input', function() {
         var telefone = $(this).val().replace(/\D/g, '');
         if (telefone.length > 0) {
             telefone = telefone.replace(/^(\d{2})(\d)/g, '($1) $2');
             telefone = telefone.replace(/(\d)(\d{4})$/, '$1-$2');
         }
         $(this).val(telefone);
-    });
+    });  
 
     $('#CPF').on('input', function() {
         var cpf = $(this).val().replace(/\D/g, '');
@@ -27,14 +27,6 @@ $(document).ready(function(){
             cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
         }
         $(this).val(cpf);
-    });
-    $('#telefone_titular').on('input', function() {
-        var telefone2 = $(this).val().replace(/\D/g, '');
-        if (telefone2.length > 0) {
-            telefone2 = telefone2.replace(/^(\d{2})(\d)/g, '($1) $2');
-            telefone2 = telefone2.replace(/(\d)(\d{4})$/, '$1-$2');
-        }
-        $(this).val(telefone2);
     });
     
 });
