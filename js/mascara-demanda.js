bff4f567030caacf1eebecaae856f5df4ecae2fa
Diego <dih.yokoyama03@gmail.com>
2024-06-11 14:39:58 -0400

    testando

--- js/mascara-demanda.js
+++ js/mascara-demanda.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-    $('#cnpj').on('input', function() {
-        var cnpj = $(this).val().replace(/\D/g, '');
-        if (cnpj.length > 0) {
-            cnpj = cnpj.replace(/^(\d{2})(\d)/, '$1.$2');
-            cnpj = cnpj.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
-            cnpj = cnpj.replace(/\.(\d{3})(\d)/, '.$1/$2');
-            cnpj = cnpj.replace(/(\d{4})(\d)/, '$1-$2');
-        }
-        $(this).val(cnpj);
-    });
+    // $('#cnpj').on('input', function() {
+    //     var cnpj = $(this).val().replace(/\D/g, '');
+    //     if (cnpj.length > 0) {
+    //         cnpj = cnpj.replace(/^(\d{2})(\d)/, '$1.$2');
+    //         cnpj = cnpj.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3');
+    //         cnpj = cnpj.replace(/\.(\d{3})(\d)/, '.$1/$2');
+    //         cnpj = cnpj.replace(/(\d{4})(\d)/, '$1-$2');
+    //     }
+    //     $(this).val(cnpj);
+    // });
 
-    $('#telefone_Pessoal/empresa').on('input', function() {
+    $('#telefone_responsavel').on('input', function() {
         var telefone = $(this).val().replace(/\D/g, '');
         if (telefone.length > 0) {
             telefone = telefone.replace(/^(\d{2})(\d)/g, '($1) $2');
--- js/validacao-cpf-cnpj-pagDemanda.js
+++ js/validacao-cpf-cnpj-pagDemanda.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('cpf' ).addEventListener('blur', validarCPF);
-    document.getElementById('cnpj' ).addEventListener('blur', validarCNPJ);
+    // document.getElementById('cnpj' ).addEventListener('blur', validarCNPJ);
 
 
 });
@@ -15,13 +15,13 @@ function validarCPF() {
     }
 }
 
-function validarCNPJ() {
-    const cnpj = document.getElementById('cnpj').value.replace(/\D/g, '');
-    if (!isCNPJValid(cnpj)) {
-        alert("CNPJ inválido! Verifique e tente novamente.");
-        document.getElementById('cnpj').value = ''; // Limpa o campo de input
-    }
-}
+// function validarCNPJ() {
+//     const cnpj = document.getElementById('cnpj').value.replace(/\D/g, '');
+//     if (!isCNPJValid(cnpj)) {
+//         alert("CNPJ inválido! Verifique e tente novamente.");
+//         document.getElementById('cnpj').value = ''; // Limpa o campo de input
+//     }
+// }
 
 function isCPFValid(strCpf) {
         if (!/[0-9]{11}/.test(strCpf)) return false;
@@ -66,46 +66,46 @@ function isCPFValid(strCpf) {
     }
     
 
-function isCNPJValid(cnpj) {
-    if (cnpj.length !== 14 ||
-        /^(\d)\1+$/.test(cnpj)) {
-        return false;
-    }
+// function isCNPJValid(cnpj) {
+//     if (cnpj.length !== 14 ||
+//         /^(\d)\1+$/.test(cnpj)) {
+//         return false;
+//     }
 
-    let length = cnpj.length - 2;
-    let numbers = cnpj.substring(0, length);
-    let digits = cnpj.substring(length);
-    let sum = 0;
-    let pos = length - 7;
+//     let length = cnpj.length - 2;
+//     let numbers = cnpj.substring(0, length);
+//     let digits = cnpj.substring(length);
+//     let sum = 0;
+//     let pos = length - 7;
 
-    for (let i = length; i >= 1; i--) {
-        sum += numbers.charAt(length - i) * pos--;
-        if (pos < 2) {
-            pos = 9;
-        }
-    }
+//     for (let i = length; i >= 1; i--) {
+//         sum += numbers.charAt(length - i) * pos--;
+//         if (pos < 2) {
+//             pos = 9;
+//         }
+//     }
 
-    let result = sum % 11 < 2 ? 0 : 11 - sum % 11;
-    if (result !== parseInt(digits.charAt(0))) {
-        return false;
-    }
+//     let result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+//     if (result !== parseInt(digits.charAt(0))) {
+//         return false;
+//     }
 
-    length += 1;
-    numbers = cnpj.substring(0, length);
-    sum = 0;
-    pos = length - 7;
+//     length += 1;
+//     numbers = cnpj.substring(0, length);
+//     sum = 0;
+//     pos = length - 7;
 
-    for (let i = length; i >= 1; i--) {
-        sum += numbers.charAt(length - i) * pos--;
-        if (pos < 2) {
-            pos = 9;
-        }
-    }
+//     for (let i = length; i >= 1; i--) {
+//         sum += numbers.charAt(length - i) * pos--;
+//         if (pos < 2) {
+//             pos = 9;
+//         }
+//     }
 
-    result = sum % 11 < 2 ? 0 : 11 - sum % 11;
-    if (result !== parseInt(digits.charAt(1))) {
-        return false;
-    }
+//     result = sum % 11 < 2 ? 0 : 11 - sum % 11;
+//     if (result !== parseInt(digits.charAt(1))) {
+//         return false;
+//     }
 
-    return true;
-}
+//     return true;
+// }
