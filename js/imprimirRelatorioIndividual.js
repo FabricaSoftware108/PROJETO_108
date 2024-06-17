ee4aac538d2abc4bedf88035219965a7a2210070
João Carlo <joaocarlofreitas@hotmail.com>
2024-06-17 17:08:46 -0400

    BOTÃO FUNCIONANDO

--- js/imprimirRelatorioIndividual.js
+++ js/imprimirRelatorioIndividual.js
@@ -1,11 +1,4 @@
-// document.getElementById('btnd').onclick = function() {
-//     window.print();
-// };
+document.getElementById('btnd').onclick = function() {
+    window.print();
+};
 
-function printFrameContent(id) {
-    alert("AAAAAAAAAAAAAA");
-    var cFrame = document.getElementById(id).contentWindow;
-    cFrame.focus();
-    cFrame.print();
-    return false;
-}
