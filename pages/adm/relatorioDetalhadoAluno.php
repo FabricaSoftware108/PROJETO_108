5e852aabf62ddfd910333eca5c24e9282850038e
Vinicius <129797873+Vrm2002@users.noreply.github.com>
2024-06-17 16:20:43 -0400

    alterando o botão x para aparecer somente para mobile
    alterando o botão x para aparecer somente para mobile

--- pages/adm/relatorioDetalhadoAluno.php
+++ pages/adm/relatorioDetalhadoAluno.php
@@ -38,7 +38,9 @@ $row = mysqli_fetch_array($result);
           <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
             <div class="offcanvas-header ">
               <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel"></h5><?php echo $_SESSION['nome']?>
-              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              <div class="offcanvas-x-button">
+              <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close" id="button-close-x"></button>
+              </div>
             </div>
             <div class="offcanvas-body">
               <ul class="navbar-nav flex-column pe-3">
