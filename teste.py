23b36fbaca070cf7f3fedac645aec52da736351d
jotafreitas <130395809+Haavertz@users.noreply.github.com>
2024-06-26 16:38:35 -0400

    Update

--- teste.py
+++ teste.py
@@ -15,54 +15,55 @@ def test_form():
         page.wait_for_timeout(5000)
         
       
-        #PAGINA CADASTRO DE VAGAS
-        page.goto('http://10.28.1.194/PROJETO_108-main/pages/editais/cadastroVagasEmprego.html') #('http://localhost/php/PROJETO_108/pages/editais/cadastroVagasEmprego.html')
-        page.wait_for_timeout(5000)
+        # #PAGINA CADASTRO DE VAGAS esta funcionando tudooo
+        # page.goto('http://10.28.1.194/PROJETO_108-main/pages/editais/cadastroVagasEmprego.html') #('http://localhost/php/PROJETO_108/pages/editais/cadastroVagasEmprego.html')
+        # page.wait_for_timeout(5000)
 
-        page.wait_for_selector('input[name="nome_empresa"]')
-        page.fill('input[name="nome_empresa"]', 'foiii amémm')
-        page.fill('input[name="email"]', 'foiAmem@email.com')
-        page.fill('input[name="cnpj"]', '87.170.176/0001-70')
-        page.fill('input[name="cidade"]', 'Campo gente')
-        page.fill('input[name="telefone"]', '67 0000-0000')
-        page.select_option('select[name=estado]', value='MS')
-        # page.fill('input[name="estado"]', 'bla')
-        page.fill('textarea[name="descricao_vaga"]', 'lorem snjnwjdnsjdsndjs nc dn nfdc fdncbdndbhdnbhjsdnjsdnsjjjjjjjj')
-        page.click('.check-consentimento-dados')
+        # page.wait_for_selector('input[name="nome_empresa"]')
+        # page.fill('input[name="nome_empresa"]', 'novoo blau')
+        # page.fill('input[name="email"]', 'foiAmem@email.com')
+        # page.fill('input[name="cnpj"]', '87.170.176/0001-70')
+        # page.fill('input[name="cidade"]', 'Campo gente')
+        # page.fill('input[name="telefone"]', '67 0000-0000')
+        # page.select_option('select[name=estado]', value='MS')
+        # # page.fill('input[name="estado"]', 'bla')
+        # page.fill('textarea[name="descricao_vaga"]', 'lorem snjnwjdnsjdsndjs nc dn nfdc fdncbdndbhdnbhjsdnjsdnsjjjjjjjj')
+        # page.click('.check-consentimento-dados')
        
         
-        page.wait_for_timeout(3000)
-        page.click('.btn.btn-primary')
-        print("Clicou no botao de enviar")
-        page.wait_for_timeout(3000)
+        # page.wait_for_timeout(3000)
+        # page.click('.btn.btn-primary')
+        # print("Clicou no botao de enviar")
+        # page.wait_for_timeout(3000)
 
         
         
-        #PAGINA BANCO DE TALENTOS, so falta arrumar o input date que não vaii
-        page.goto('http://localhost/PROJETO_108/pages/editais/pagBancoTalentos.html')
+        #PAGINA BANCO DE TALENTOS, so falta arrumar o input date que não vaii nao esta funcionando  tudoo
+        page.goto('http://10.28.1.194/PROJETO_108-main/pages/editais/pagBancoTalentos.html')
         page.wait_for_timeout(5000)
         
         page.wait_for_selector('input[name="nome"]')
         page.fill('input[name=nome]', 'Adam Carsen')    
         page.fill('input[name=cpf]', '618.149.190-23')
-        page.get_by_label("Data de Nascimento").fill("2020-02-02")
+        # page.click('.cadastrovagas.col-md-5.col-10.col-sm-12')
         # page.fill('input[name=dataNascimento]', '2020-02-02')
         page.select_option('select[name=escolaridade]', value='Superior')
-        page.fill('input[name=telefone]', '90000-0000') 
-        page.fill('input[name=email]', 'costhelaria@gmail.com')
-        page.fill("input[name=cep]","79017253")
+        page.fill('input[name=telefone]', '6656454566') 
+        page.fill('input[name=email]', 'adam@gmail.com')
+        page.fill("input[name=cep]","75087253")
         page.fill('input[name=numero]', '543')
         page.fill('input[name=rua]', 'Rua da Alegria')
-        page.fill('input[name=bairro]', '01 dos bairros')
+        page.fill('input[name=bairro]', ' bairros')
         page.fill('input[name=cidade]', 'Campo Pequeno')
+        page.select_option('select[name=uf]', value='Paraná')
         page.fill('input[name=github]', 'mnjnjnn')
         page.fill('input[name=linkedin]', 'Linikernjnjc')
         page.click('.check-consentimento-dados')
         
         
-        page.wait_for_timeout(5000)
+        page.wait_for_timeout(2000)
         page.click('.btn.btn-primary')
-        page.wait_for_timeout(5000)
+        page.wait_for_timeout(2000)
         
        #PAG ALUNO PSG
        
@@ -99,25 +100,25 @@ def test_form():
         
         
         
-        #PAG EMPRESAS
-        page.goto('http://10.28.1.194/PROJETO_108-main/pages/editais/pagDemanda.html')
-        page.wait_for_timeout(5000)
+        # #PAG EMPRESAS
+        # page.goto('http://10.28.1.194/PROJETO_108-main/pages/editais/pagDemanda.html')
+        # page.wait_for_timeout(5000)
         
-        page.wait_for_selector('input[name="nome"]')
-        page.fill('input[name=nome]', ' Bianca')    
-        page.fill('input[name=email]', 'bianca@gmail.com')
-        page.fill('input[name=cpf]', '618.149.190-23')
-        page.fill('input[name=telefone]', '265656') 
-        page.fill('input[name=cidade]', 'Amambai')
-        page.select_option('select[name=estado]', value='MT')
-        page.fill('input[name=descricao]', 'mkfmfkmfksmkmfkmfkfnmdkfdknfkkmnd')
-        page.click('.check-consentimento-dados')
+        # page.wait_for_selector('input[name="nome"]')
+        # page.fill('input[name=nome]', ' Bianca')    
+        # page.fill('input[name=email]', 'bianca@gmail.com')
+        # page.fill('input[name=cpf]', '618.149.190-23')
+        # page.fill('input[name=telefone]', '265656') 
+        # page.fill('input[name=cidade]', 'Amambai')
+        # page.select_option('select[name=estado]', value='MT')
+        # page.fill('input[name=descricao]', 'mkfmfkmfksmkmfkmfkfnmdkfdknfkkmnd')
+        # page.click('.check-consentimento-dados')
         
-        page.wait_for_timeout(3000)
+        # page.wait_for_timeout(3000)
         
-        page.click('.btn.btn-primary')
+        # page.click('.btn.btn-primary')
         
-        page.wait_for_timeout(3000)
+        # page.wait_for_timeout(3000)
         
         
         
