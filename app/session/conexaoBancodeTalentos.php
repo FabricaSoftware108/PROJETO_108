1bc53fea1f6dd22be145562f149b8cdf02825347
Niicolas <nicollasrubini@gmail.com>
2024-06-28 17:29:26 -0400

    update

--- app/session/conexaoBancodeTalentos.php
+++ app/session/conexaoBancodeTalentos.php
@@ -4,34 +4,38 @@ session_start();
 include '../db/connection.php';
 
 // Verifica se os valores foram recebidos do formulário antes de usá-los
-if (!empty($_POST["nome"]) && !empty($_POST["cep"]) && !empty($_POST["cpf"]) && !empty($_POST["escolaridade"]) && !empty($_POST["dataNascimento"]) && !empty($_POST["email"]) && !empty($_POST["telefone"]) && !empty($_POST["uf"]) && !empty($_POST["rua"]) && !empty($_POST["bairro"]) && !empty($_POST["cidade"]) && !empty($_POST["numero"]) && !empty($_POST["github"]) && !empty($_POST["linkedin"])) {
-    $nome = mysqli_real_escape_string($connection, $_POST["nome"]);
-    $cep = mysqli_real_escape_string($connection, $_POST["cep"]);
-    $cpf = mysqli_real_escape_string($connection, $_POST["cpf"]);
-    $escolaridade = mysqli_real_escape_string($connection, $_POST["escolaridade"]);
-    $dataNascimento = mysqli_real_escape_string($connection, $_POST["dataNascimento"]);
-    $email = mysqli_real_escape_string($connection, $_POST["email"]);
-    $telefone = mysqli_real_escape_string($connection, $_POST["telefone"]);
-    $estado = mysqli_real_escape_string($connection, $_POST["uf"]);
-    $rua = mysqli_real_escape_string($connection, $_POST["rua"]);
-    $bairro = mysqli_real_escape_string($connection, $_POST["bairro"]);
-    $cidade = mysqli_real_escape_string($connection, $_POST["cidade"]);
-    $numero = mysqli_real_escape_string($connection, $_POST["numero"]);
-    $github = mysqli_real_escape_string($connection, $_POST["github"]);
-    $linkedin = mysqli_real_escape_string($connection, $_POST["linkedin"]);
+if (empty($_POST["nome"]) && empty($_POST["cep"]) && empty($_POST["cpf"]) && empty($_POST["escolaridade"]) && empty($_POST["dataNascimento"]) && empty($_POST["email"]) && 
+    empty($_POST["telefone"]) && empty($_POST["uf"]) && empty($_POST["rua"]) && empty($_POST["bairro"]) && empty($_POST["cidade"]) && empty($_POST["numero"]) && 
+    empty($_POST["github"]) && empty($_POST["linkedin"])) {
+
+
+    echo "<script language='javascript'>window.alert('Não foi possível efetuar o cadastro'); window.location.href='../../pages/editais/pagBancoTalentos.html';</script>";
+
+
+} else {
+    
+    $nome = $_POST["nome"];
+    $cep = $_POST["cep"];
+    $cpf = $_POST["cpf"];
+    $escolaridade = $_POST["escolaridade"];
+    $dataNascimento = $_POST["dataNascimento"];
+    $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+    $estado = $_POST["uf"];
+    $rua = $_POST["rua"];
+    $bairro = $_POST["bairro"];
+    $cidade = $_POST["cidade"];
+    $numero = $_POST["numero"];
+    $github = $_POST["github"];
+    $linkedin = $_POST["linkedin"];
 
     $query = "INSERT INTO bancosTalentos (cpf, nome, cep, escolaridade, dataNascimento, email, telefone, estado, cidade, bairro, rua, numero, github, linkedin) 
               VALUES ('$cpf', '$nome', '$cep', '$escolaridade', '$dataNascimento', '$email', '$telefone', '$estado', '$cidade', '$bairro', '$rua', '$numero', '$github', '$linkedin')";
 
     $result = mysqli_query($connection, $query);
-
-    if (!$result) {
-        echo "<script language='javascript'>window.alert('Não foi possível efetuar o cadastro'); window.location.href='../../pages/editais/pagBancoTalentos.html    ';</script>";
-    } else {
-        echo "<script language='javascript'>window.alert('Cadastro efetuado com sucesso'); window.location.href='../../pages/editais/pagBancoTalentos.html';</script>";
-    }
-} else {
-    echo "<script language='javascript'>window.alert('Por favor, preencha todos os campos do formulário'); window.location.href='../../pages/editais/pagBancoTalentos.html';</script>";
-}
+    
+    echo "<script language='javascript'>window.alert('Cadastro efetuado com sucesso'); window.location.href='../../pages/editais/pagBancoTalentos.html';</script>";
+} 
+// echo "<script language='javascript'>window.alert('Por favor, preencha todos os campos do formulário'); window.location.href='../../pages/editais/pagBancoTalentos.html';</script>";
 
 exit();
