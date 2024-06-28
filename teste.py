46735219b005bf904b44605c38970ca13cf8a795
Ana <alphawolf244@gmail.com>
2024-06-28 16:42:28 -0400

    update

--- teste.py
+++ teste.py
@@ -1,7 +1,4 @@
 
-
-
-
 from playwright.sync_api import sync_playwright
 
 
@@ -15,57 +12,60 @@ def test_form():
         page.wait_for_timeout(5000)
         
       
-        # #PAGINA CADASTRO DE VAGAS esta funcionando tudooo
-        # page.goto('http://10.28.1.194/PROJETO_108-main/pages/editais/cadastroVagasEmprego.html') #('http://localhost/php/PROJETO_108/pages/editais/cadastroVagasEmprego.html')
-        # page.wait_for_timeout(5000)
-
-        # page.wait_for_selector('input[name="nome_empresa"]')
-        # page.fill('input[name="nome_empresa"]', 'novoo blau')
-        # page.fill('input[name="email"]', 'foiAmem@email.com')
-        # page.fill('input[name="cnpj"]', '87.170.176/0001-70')
-        # page.fill('input[name="cidade"]', 'Campo gente')
-        # page.fill('input[name="telefone"]', '67 0000-0000')
-        # page.select_option('select[name=estado]', value='MS')
-        # # page.fill('input[name="estado"]', 'bla')
-        # page.fill('textarea[name="descricao_vaga"]', 'lorem snjnwjdnsjdsndjs nc dn nfdc fdncbdndbhdnbhjsdnjsdnsjjjjjjjj')
-        # page.click('.check-consentimento-dados')
-       
-        
-        # page.wait_for_timeout(3000)
-        # page.click('.btn.btn-primary')
-        # print("Clicou no botao de enviar")
-        # page.wait_for_timeout(3000)
-
-        
-        
-        #PAGINA BANCO DE TALENTOS, so falta arrumar o input date que não vaii nao esta funcionando  tudoo
-        page.goto('http://127.0.0.1:5502/pages/editais/pagBancoTalentos.html')
+        #PAGINA CADASTRO DE VAGAS esta funcionando tudooo
+        page.goto('http://127.0.0.1:5502/pages/editais/cadastroVagasEmprego.html') #('http://localhost/php/PROJETO_108/pages/editais/cadastroVagasEmprego.html')
         page.wait_for_timeout(5000)
-        
-        page.wait_for_selector('input[name="nome"]')
-        page.fill('input[name=nome]', 'Adam Carsen')    
-        page.fill('input[name=cpf]', '618.149.190-23')
-        page.wait_for_timeout(1000)
-        page.click('.cadastrovagas.col-md-5.col-10.col-sm-12')
-        page.fill('input[name=dataNascimento]', )
-        page.wait_for_timeout(1000)
-        page.select_option('select[name=escolaridade]', value='Superior')
-        page.fill('input[name=telefone]', '6655454566') 
-        page.fill('input[name=email]', 'adam@gmail.com')
-        page.fill("input[name=cep]","79017253")
-        page.fill('input[name=numero]', '543')
-        page.select_option('select[name=uf]', value='AL')
-        
-        page.wait_for_timeout(1000)
-       
-        page.fill('input[name=github]', 'lllll')
-        page.fill('input[name=linkedin]', 'mkjk')
+
+        page.wait_for_selector('input[name="nome_empresa"]')
+        page.fill('input[name="nome_empresa"]', 'novoo blau')
+        page.fill('input[name="email"]', 'foiAmem@email.com')
+        page.fill('input[name="cnpj"]', '87.170.176/0001-70')
+        page.fill('input[name="cidade"]', 'Campo gente')
+        page.fill('input[name="telefone"]', '67 0000-0000')
+        page.select_option('select[name=estado]', value='MS')
+        # page.fill('input[name="estado"]', 'bla')
+        page.fill('textarea[name="descricao_vaga"]', 'lorem snjnwjdnsjdsndjs nc dn nfdc fdncbdndbhdnbhjsdnjsdnsjjjjjjjj')
         page.click('.check-consentimento-dados')
+       
         
-        
-        page.wait_for_timeout(2000)
+        page.wait_for_timeout(3000)
         page.click('.btn.btn-primary')
-        page.wait_for_timeout(2000)
+        print("Clicou no botao de enviar")
+        page.wait_for_timeout(1000)
+
+        
+        
+        # #PAGINA BANCO DE TALENTOS, so falta arrumar o input date que não vaii nao esta funcionando  tudoo
+        # page.goto('http://127.0.0.1:5502/pages/editais/pagBancoTalentos.html')
+        # page.wait_for_timeout(5000)
+        
+        # page.wait_for_selector('input[name="nome"]')
+        # page.fill('input[name=nome]', 'Adam Carsen')    
+        # page.fill('input[name=cpf]', '618.149.190-23')
+        # page.fill('input[name=dataNascimento]','8-56-2023')
+        # # page.wait_for_timeout(1000)
+        # # page.click('.cadastrovagas.col-md-5.col-10.col-sm-12')
+        # # page.wait_for_timeout(1000)
+
+
+        # # page.wait_for_timeout(3000)
+      
+        # # page.wait_for_timeout(1000)
+        # page.select_option('select[name=escolaridade]', value='Superior')
+        # page.fill('input[name=telefone]', '6655454566') 
+        # page.fill('input[name=email]', 'adam@gmail.com')
+        # page.fill("input[name=cep]","79017253")
+        # page.fill('input[name=numero]', '543')
+        # page.select_option('select[name=uf]', value='AL')
+        
+        # page.fill('input[name=github]', '6655454566') 
+        # page.fill('input[name=linkedin]', 'adam@gmail.com')
+        
+        # page.click('.check-consentimento-dados')
+        
+        # # page.wait_for_timeout(2000)
+        # page.click('.btn.btn-primary')
+        # page.wait_for_timeout(2000)
         
        #PAG ALUNO PSG
        
@@ -102,34 +102,27 @@ def test_form():
         
         
         
-        # #PAG EMPRESAS
-        # page.goto('http://10.28.1.194/PROJETO_108-main/pages/editais/pagDemanda.html')
-        # page.wait_for_timeout(5000)
+        #PAG EMPRESAS
+        page.goto('http://127.0.0.1:5502/PROJETO_108/pages/editais/pagDemanda.html')
+        page.wait_for_timeout(5000)
         
-        # page.wait_for_selector('input[name="nome"]')
-        # page.fill('input[name=nome]', ' Bianca')    
-        # page.fill('input[name=email]', 'bianca@gmail.com')
-        # page.fill('input[name=cpf]', '618.149.190-23')
-        # page.fill('input[name=telefone]', '265656') 
-        # page.fill('input[name=cidade]', 'Amambai')
-        # page.select_option('select[name=estado]', value='MT')
-        # page.fill('input[name=descricao]', 'mkfmfkmfksmkmfkmfkfnmdkfdknfkkmnd')
-        # page.click('.check-consentimento-dados')
+        page.wait_for_selector('input[name="nome"]')
+        page.fill('input[name=nome]', ' Bianca')    
+        page.fill('input[name=email]', 'bianca@gmail.com')
+        page.fill('input[name=cpf]', '618.149.190-23')
+        page.fill('input[name=telefone]', '265656') 
+        page.fill('input[name=cidade]', 'Amambai')
+        page.select_option('select[name=estado]', value='MT')
+        page.fill('input[name=descricao]', 'mkfmfkmfksmkmfkmfkfnmdkfdknfkkmnd')
+        page.click('.check-consentimento-dados')
         
-        # page.wait_for_timeout(3000)
+        page.wait_for_timeout(3000)
         
-        # page.click('.btn.btn-primary')
+        page.click('.btn.btn-primary')
         
-        # page.wait_for_timeout(3000)
+        page.wait_for_timeout(3000)
         
-        
-        
-        
-        
-        
-        
-        
-        
+    
         
        
         
