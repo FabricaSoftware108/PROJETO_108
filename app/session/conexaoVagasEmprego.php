42ce35d4620ef213e912a8f03a05679a53d8d8ce
Gabriel Gravena Barros <contatoluisfernandogaleano@gmail.com>
2024-07-02 16:43:10 -0400

    Formulariso de input do estado arrumado 100%

--- app/session/conexaoVagasEmprego.php
+++ app/session/conexaoVagasEmprego.php
@@ -2,14 +2,16 @@
 
 include '../db/connection.php';
 
-if (isset($_POST['nome_empresa']) && 
-    isset($_POST['email']) && 
-    isset($_POST['cnpj']) && 
-    isset($_POST['cidade']) && 
-    isset($_POST['telefone']) && 
-    isset($_POST['estado']) && 
-    isset($_POST['descricao_vaga'])){
-        
+if (
+    isset($_POST['nome_empresa']) &&
+    isset($_POST['email']) &&
+    isset($_POST['cnpj']) &&
+    isset($_POST['cidade']) &&
+    isset($_POST['telefone']) &&
+    isset($_POST['estado']) &&
+    isset($_POST['descricao_vaga'])
+) {
+
     $nome_empresa = $_POST['nome_empresa'];
     $email = $_POST['email'];
     $cnpj = $_POST['cnpj'];
@@ -19,9 +21,9 @@ if (isset($_POST['nome_empresa']) &&
     $descricao_vaga = $_POST['descricao_vaga'];
 
     $query = "INSERT INTO vagasEmprego (cnpj, nomeEmpresa, emailEmpresa, cidade, telefone, estado, descricao) VALUES ('$cnpj','$nome_empresa', '$email', '$cidade', '$telefone', '$estado', '$descricao_vaga')";
-    
+
     $result = mysqli_query($connection, $query);
-    
+
     if (!$result) {
         echo "<script language='javascript'>window.alert('Não foi possível efetuar o cadastro'); window.location.href='../../pages/editais/cadastroVagasEmprego.html    ';</script>";
     } else {
@@ -30,5 +32,3 @@ if (isset($_POST['nome_empresa']) &&
 } else {
     echo "<script language='javascript'>window.alert('Por favor, preencha todos os campos do formulário'); window.location.href='../../pages/editais/cadastroVagasEmprego.html';</script>";
 }
-
-?>
