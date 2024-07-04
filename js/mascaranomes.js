03787eeb1f051a5ef0eb9bcc72c601b12b45e656
H a a v e r t z <gleisonmorais06@gmail.com>
2024-07-03 23:39:30 -0300

    Mascara CadastroVagasEmprego

--- js/mascaranomes.js
+++ js/mascaranomes.js
@@ -1,7 +1,6 @@
 function aplicarMascaraNome(input) {
     input.addEventListener('input', function () {
         let valor = input.value;
-        // Permitir apenas letras e espaços
         input.value = valor.replace(/[^a-zA-ZÀ-ÿ\s]/g, '');
     });
 }
@@ -10,4 +9,16 @@ document.addEventListener('DOMContentLoaded', function () {
     aplicarMascaraNome(document.getElementById('nome'));
     aplicarMascaraNome(document.getElementById('nomePai'));
     aplicarMascaraNome(document.getElementById('nomeMae'));
+});
+
+
+function aplicarMascaraNome(input) {
+    input.addEventListener('input', function() {
+        let valor = input.value;
+        input.value = valor.replace(/[^a-zA-ZÀ-ÿ\s]/g, '');
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    aplicarMascaraNome(document.getElementById('nome_empresa'));
 });
