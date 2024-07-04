c45572c597e4ce813259b2d0c09db24fe2195f57
Pedroca <pedroca2787@gmail.com>
2024-07-04 13:53:57 -0400

    update

--- app/session/vendor/composer/autoload_static.php
+++ app/session/vendor/composer/autoload_static.php
@@ -12,6 +12,10 @@ class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
             'Svg\\' => 4,
             'Sabberworm\\CSS\\' => 15,
         ),
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
         'M' => 
         array (
             'Masterminds\\' => 12,
@@ -34,6 +38,10 @@ class ComposerStaticInit2185d2f99bcd56787481d9357a5972d3
         'Sabberworm\\CSS\\' => 
         array (
             0 => __DIR__ . '/..' . '/sabberworm/php-css-parser/src',
+        ),
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
         'Masterminds\\' => 
         array (
