631bcffedf200524fcbbc24ef8bf82c634465427
Pedroca <pedroca2787@gmail.com>
2024-07-17 17:09:33 -0400

    update

--- js/devCards.js
+++ js/devCards.js
@@ -1,60 +1,47 @@
-// Pedro Diniz Santana
+var devColors = {
+    pedro: "#5B5298",
+    larissa: "#293E3B",
+    gleisson: "#8C817D",
+    luis: "#131719",
+    igor: "#162945",
+    joao: "#5E452D",
+    ana: "#BAAAC3",
+    gravena: "#88A7AC",
+    nicolas: "#A38CE8",
+    romano: "#948574",
+    hentony: "#9F9699",
+    christian: "#00A2D8",
+    diego: "#3F648E",
+    vinicius: "#4F6745",
+    evandro: "#1E4879",
+    pontes: "#4F6745",
+    cantico: "#8A5731",
+    jheron: "#4F6745",
+    matheus: "#4F6745",
+}
 
-document.querySelector('.dev1').addEventListener("mouseenter", function(){
-    document.querySelector(".dev1").style.border = "#CC3E52 solid 1px";
-    document.querySelector("#devSocialBg1").style.background = "#CC3E52";
-    for(var i = 0; i < document.querySelectorAll("#devSocialBg1 a").length; i++){
-        document.querySelectorAll("#devSocialBg1 a")[i].style.color = "white";
+var divs = []
 
-    }
+for(var i = 0; i < document.getElementsByClassName("devCard").length ; i++){
+    divs.push(document.getElementsByClassName("devCard")[i])
+}
 
-})
-document.querySelector('.dev1').addEventListener("mouseleave", function(){
-    document.querySelector(".dev1").style.border = "none";
-    document.querySelector("#devSocialBg1").style.background = "#f1f1f1";
-    for(var i = 0; i < document.querySelectorAll("#devSocialBg1 a").length; i++){
-        document.querySelectorAll("#devSocialBg1 a")[i].style.color = "black";
+divs.forEach(function (element, index) {
+    element.addEventListener("mouseenter", function(){
+        element.style.border = `${Object.values(devColors)[index]} solid 1px`;
+        element.querySelector("#devSocialBg").style.background = `${Object.values(devColors)[index]}`;
+        for(var i = 0; i < element.querySelectorAll("#devSocialBg a").length; i++){
+            element.querySelectorAll("#devSocialBg a")[i].style.color = "white";
+            
+        }
+    })
+    element.addEventListener("mouseleave", function(){
+        element.style.border = "none";
+        element.querySelector("#devSocialBg").style.background = "#f1f1f1";
+        for(var i = 0; i < element.querySelectorAll("#devSocialBg a").length; i++){
+            element.querySelectorAll("#devSocialBg a")[i].style.color = "black";
+            
+        }
+    })
+});
 
-    }
-})
-
-//Larissa
-
-document.querySelector('.dev2').addEventListener("mouseenter", function(){
-    document.querySelector(".dev2").style.border = "#344E44 solid 1px";
-    document.querySelector("#devSocialBg2").style.background = "#344E44";
-    for(var i = 0; i < document.querySelectorAll("#devSocialBg1 a").length; i++){
-        document.querySelectorAll("#devSocialBg2 a")[i].style.color = "white";
-
-    }
-
-})
-document.querySelector('.dev2').addEventListener("mouseleave", function(){
-    document.querySelector(".dev2").style.border = "none";
-    document.querySelector("#devSocialBg2").style.background = "#f1f1f1";
-    for(var i = 0; i < document.querySelectorAll("#devSocialBg1 a").length; i++){
-        document.querySelectorAll("#devSocialBg2 a")[i].style.color = "black";
-
-    }
-})
-
-
-//Gleisson
-
-document.querySelector('.dev3').addEventListener("mouseenter", function(){
-    document.querySelector(".dev3").style.border = "#8B8178 solid 1px";
-    document.querySelector("#devSocialBg3").style.background = "#8B8178";
-    for(var i = 0; i < document.querySelectorAll("#devSocialBg1 a").length; i++){
-        document.querySelectorAll("#devSocialBg3 a")[i].style.color = "white";
-
-    }
-
-})
-document.querySelector('.dev3').addEventListener("mouseleave", function(){
-    document.querySelector(".dev3").style.border = "none";
-    document.querySelector("#devSocialBg3").style.background = "#f1f1f1";
-    for(var i = 0; i < document.querySelectorAll("#devSocialBg1 a").length; i++){
-        document.querySelectorAll("#devSocialBg3 a")[i].style.color = "black";
-
-    }
-})
